897c097025379d4e5cc78067a0b8417150e25c09
yessenia <yesseniaperez.n5@gmail.com>
2024-08-18 15:26:38 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Yessenia Martin";
+    int age = 28;
+    String state = "Texas";
+
+    System.out.println(name + " " + age + " " + state);
+  }
+}
