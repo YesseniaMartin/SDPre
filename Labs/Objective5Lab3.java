c7ca1366551b186cf75a33ce17c8a4fb7d72b210
yessenia <yesseniaperez.n5@gmail.com>
2024-08-20 12:02:59 -0600

    complete objective5lab3

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Objective5Lab3 {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      
+
+      System.out.println("Please enter a number: ");
+      int userNum = scanner.nextInt();
+
+      if (userNum >= 0) {
+        System.out.println("The number is positive");
+      }
+
+      else if (userNum < 0) {
+        System.out.println("The number is negative");
+      }
+
+      /*
+        Your solution goes here
+      */
+
+      scanner.close();
+  }
+}
