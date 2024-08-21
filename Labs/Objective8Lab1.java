a97053c8cca5d312a5e264d48b4f7804b135d62f
yessenia <yesseniaperez.n5@gmail.com>
2024-08-21 14:56:56 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,15 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+
+    //TODO Write a for loop that will print the numbers 1 to 10
+    for (int i=0; i<11; i+=1){
+    System.out.println(i);
+    }
+
+    //TODO Write another for loop that will print the numbers 10 to 1.
+    for (int h=10; h>0; h-=1){
+      System.out.println(h);
+    }
+  }
+}
