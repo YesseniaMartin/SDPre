d7e274bdeaed3e8ba79605b774c00aeea0b69bc7
yessenia <yesseniaperez.n5@gmail.com>
2024-08-21 10:25:37 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int sumTotal = 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 16 + 17 + 18 + 19 + 20 + 21;
+
+    while(counter <= 1) {
+    System.out.println(sumTotal);
+    
+    counter = counter + 1;
+    }
+  }
+}
