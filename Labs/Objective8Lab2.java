28e166f485e2ef30dec867ab692e06a3cafec357
yessenia <yesseniaperez.n5@gmail.com>
2024-08-21 20:36:58 -0600

    complete Objective8Lab3

--- Labs/Objective8Lab2.java
+++ Labs/Objective8Lab2.java
@@ -1,10 +1,12 @@
 public class Objective8Lab2 {
 	public static void main(String[] args) {
 
-		int total = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19 + 20;
+		int sum = 0;
 
-		for (int i=0; i<1; i+=1){
-			System.out.println(total);
+		for (int i=0; i <= 20; i+=1){
+
+			sum = sum + i;
 		}
+		System.out.println(sum);
 	}
 }
